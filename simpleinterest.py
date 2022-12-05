4d1796524d33817365f25d6a29ab7714b493dbfc
Akshay prem <62068319+Akshay-prem@users.noreply.github.com>
2022-12-05 18:23:50 +0530

    Add files via upload

---
+++ simpleinterest.py
@@ -0,0 +1,6 @@
+deposit=int(input("Enter initial amount\n"))
+rate=int(input("Enter interest rate\n"))
+time=int(input("enter units of time\n"))
+Si=(deposit*rate*time)/100
+print("the simple interest obtained is "+str(Si))
+
